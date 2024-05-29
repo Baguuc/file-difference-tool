2567725790f4b97ede32297b0184ecd4b715c229
Baguuc <janzukowski@proton.me>
2024-05-29 17:40:11 +0200

    update: release 0.1.0

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
+use std::{fs, path::Path};
+
+use serde::Serialize;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 fn scan_directory(directory: String) -> Vec<String> {
-    let mut read: Vec<String> = vec![];
-    fs::read_dir(directory).unwrap().for_each(|file| {
+    let mut readed: Vec<String> = vec![];
+    let read = fs::read_dir(directory);
+
+    if read.is_err() {
+        return vec![];
+    }
+
+    let read = read.unwrap();
+
+    read.for_each(|file| {
         if file.is_err() {
             return;
         }
-        
+
+        if file.is_err() {
+            return;
+        }
+
         let binding = file.unwrap().file_name();
         let filename = binding.to_str();
 
@@ -18,49 +32,79 @@ fn scan_directory(directory: String) -> Vec<String> {
             return;
         }
 
-        let binding = filename.unwrap().to_string();
-        let mut filename = binding.split(".").collect::<Vec<&str>>();
-        filename.pop();
-        let filename = filename.join(".");
+        let filename = filename.unwrap().to_string();
 
-        read.push(filename);
+        readed.push(filename);
     });
 
-    return read;
+    return readed;
+}
+
+
+#[derive(Debug, Serialize)]
+struct FileDifferenceItem {
+    filename: String,
+    exclusive_to: Option<String>
 }
 
 
 #[tauri::command]
-fn get_differences(directories: Vec<&str>) -> Vec<Vec<String>> {
+fn get_differences(directories: Vec<&str>) -> Vec<FileDifferenceItem> {
     let files1 = scan_directory(directories[0].to_string());
     let files2 = scan_directory(directories[1].to_string());
 
-    let mut f1_diff: Vec<String> = vec![];
-    let mut f2_diff: Vec<String> = vec![];
+    let mut differences: Vec<FileDifferenceItem> = vec![];
 
     for filename in files1.clone() {
         if files2.contains(&filename) {
+            if differences.iter().any(|x| x.filename == filename) {
+                continue;
+            }
+
+            differences.push(FileDifferenceItem {
+                filename,
+                exclusive_to: None
+            });
             continue;
         }
         
-        f1_diff.push(filename);
+        differences.push(FileDifferenceItem {
+            filename,
+            exclusive_to: Some("f1".to_string())
+        });
     }
 
     for filename in files2.clone() {
-        if files1.contains(&filename) {
+        if differences.iter().any(|x| x.filename == filename) {
             continue;
         }
         
-        f2_diff.push(filename);
+        if files1.contains(&filename) {
+            differences.push(FileDifferenceItem {
+                filename,
+                exclusive_to: None
+            });
+            continue;
+        }
+        
+        differences.push(FileDifferenceItem {
+            filename,
+            exclusive_to: Some("f2".to_string())
+        });
     }
     
-    return vec![f1_diff, f2_diff];
+    return differences;
 }
 
 
+#[tauri::command]
+fn dir_exists(directory: String) -> bool {
+    Path::new(directory.as_str()).exists()
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_differences])
+        .invoke_handler(tauri::generate_handler![get_differences, dir_exists])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,143 @@
-// When using the Tauri API npm package:
 import { invoke } from "@tauri-apps/api/tauri";
 
-const scanBtn = document.getElementById("scan-btn");
-const f1 = document.getElementById("f1") as any;
-const f2 = document.getElementById("f2") as any;
-const f1_diff_elem = document.getElementById("f1_diff");
-const f2_diff_elem = document.getElementById("f2_diff");
+const f1 = document.getElementById("f1") as HTMLInputElement;
+const f2 = document.getElementById("f2") as HTMLInputElement;
+const differencesListElement = document.getElementById(
+  "differences"
+) as HTMLUListElement;
 
-// Invoke the command
+/*
+############################
+#                          #
+#   Function definitions   #
+#                          #
+############################
+*/
+
+interface FileListItem {
+  filename: string;
+  exclusive_to: "f1" | "f2" | null;
+}
+
 async function scan() {
-  f1_diff_elem!.innerHTML = "";
-  f2_diff_elem!.innerHTML = "";
+  // make sure to remove previous results list
+  differencesListElement.innerHTML = "";
 
+  // make sure both fields are filled
   if (!f1.value || !f2.value) {
     return;
   }
 
-  const differences: string[][] = await invoke("get_differences", {
-    directories: [f1.value, f2.value],
+  const folders = [f1.value, f2.value];
+
+  // scan the folders for differences
+  const differences: FileListItem[] = await invoke("get_differences", {
+    directories: folders,
   });
 
-  const f1_diff = differences[0] || [];
-  const f2_diff = differences[1] || [];
+  differences.sort((prev, current) => {
+    // just so you dont have to read:
+    // this code just sorts the list with this priority:
+    //
+    // files that are exclusive to f1 < this is most prioritized
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // files that are exclusive to f2
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // files that are not exclusive to any folder
 
-  f1_diff.forEach((filename: string) => {
-    const listitem = document.createElement("li");
-    listitem.textContent = filename;
-    f1_diff_elem?.appendChild(listitem);
+    if (prev.exclusive_to && current.exclusive_to) {
+      if (prev.exclusive_to === "f2" && current.exclusive_to === "f1") {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else if (prev.exclusive_to) {
+      return -1;
+    } else {
+      return 1;
+    }
   });
 
-  f2_diff.forEach((filename: string) => {
+  if (differences.length === 0) {
     const listitem = document.createElement("li");
-    listitem.textContent = filename;
-    f2_diff_elem?.appendChild(listitem);
+    listitem.textContent = "No files found";
+    listitem.className = "file-difference-item";
+    listitem.style.border = "1px solid var(--color-error)";
+    listitem.style.background = "none";
+    listitem.style.justifyContent = "center";
+
+    return;
+  }
+
+  console.log(differences);
+
+  // display the results in the list
+  differences.forEach((item) => {
+    const listitem = document.createElement("li");
+    listitem.className = "file-difference-item";
+
+    const filenameElement = document.createElement("span");
+    filenameElement.textContent = item.filename;
+    listitem.appendChild(filenameElement);
+
+    const exclusiveToElement = document.createElement("span");
+    exclusiveToElement.className = "folder-icon folder-icon-text";
+
+    // if item is exclusive to some folder, then add a folder mark for user to know where it belongs
+    if (item.exclusive_to) {
+      exclusiveToElement.className +=
+        item.exclusive_to === "f1" ? " folder1-icon" : " folder2-icon";
+    } else {
+      exclusiveToElement.className += " folder-both-icon";
+    }
+
+    listitem.appendChild(exclusiveToElement);
+
+    differencesListElement?.appendChild(listitem);
   });
 }
 
-scanBtn?.addEventListener("click", scan);
+/*
+#####################################
+#                                   #
+#   Form validation and handling    #
+#                                   #
+#####################################
+*/
+
+const scanPathsForm = document.getElementById(
+  "scan-paths-form"
+) as HTMLFormElement;
+
+scanPathsForm.querySelectorAll('input[type="text"]').forEach((input) => {
+  input.addEventListener("input", async (event: Event) => {
+    event.preventDefault();
+    const target = event.target as HTMLInputElement;
+    const isInputEmpty = (event.target as HTMLInputElement).value === "";
+
+    if (isInputEmpty) {
+      target.style.borderColor = "var(--color-error)";
+
+      return;
+    } else {
+      target.style.borderColor = "var(--color-text)";
+    }
+
+    const valid = await invoke("dir_exists", {
+      directory: target.value,
+    });
+
+    if (!valid) {
+      target.style.borderColor = "var(--color-error)";
+
+      return;
+    } else {
+      target.style.borderColor = "var(--color-text)";
+    }
+  });
+});
+
+scanPathsForm?.addEventListener("submit", (event: SubmitEvent) => {
+  event.preventDefault();
+  scan();
+});
